9229bf71d406c13eb6dfba1a37a15d9ab1f94f9a
Anali Silva <b.anali.silva@gmail.com>
2022-06-19 15:03:37 -0300

    feat: adicionado os métodos get por id, put e delete na controllers e retirado ';' que estavam a mais nos outros arquivos.

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -7,6 +7,16 @@ class LivroController {
             res.status(200).json(livros)
         })
     }
+    static listarLivroPorId = (req, res) => {
+        const id = req.params.id;
+        livros.findById(id, (erro, livros)=> {
+            if(erro) {
+                res.status(400).send({message: `${erro.message} - Id do livro não localizado`})
+            }else{
+                res.status(200).send(livros)
+            }
+        })
+    }
     static cadastrarLivro = (req, res) => {
         let livro = new livros(req.body);
         livro.save((erro) =>{
@@ -17,6 +27,28 @@ class LivroController {
             }
         })
     }
+    static atualizarLivro = (req, res) => {
+        const id = req.params.id;
+
+        livros.findByIdAndUpdate(id, {$set: req.body}, (erro)=> {
+            if(!erro){
+                res.status(200).send({message: "Livro atualizado com sucesso."})
+            }else{
+                res.status(500).send({message: erro.message})
+            }
+        })
+    }
+    static excluirLivro = (req, res) => {
+        const id = req.params.id;
+
+        livros.findByIdAndDelete(id, (erro)=>{
+            if(!erro){
+                res.status(200).send({message: "Livro removido com sucesso"})
+            }else{
+                res.status(500).send({message: erro.message})
+            }
+        })
+    }
 }
 
-export default LivroController;
+export default LivroController
--- src/models/Livro.js
+++ src/models/Livro.js
@@ -11,4 +11,4 @@ const livroSchema = new mongoose.Schema(
 
 const livros = mongoose.model('livros', livroSchema);
 
-export default livros;
+export default livros
